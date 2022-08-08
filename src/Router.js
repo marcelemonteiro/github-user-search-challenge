e26ef66910854682fbad33d6b0e65e11de3dddf4
Marcele Monteiro <marcelepmonteiro@gmail.com>
2022-08-08 20:27:30 -0300

    Remove BrowserRouter and Add HashRouter

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import GithubUserSearch from "./pages/GithubUserSearch";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route
-          path="github-user-search-challenge"
-          element={<GithubUserSearch />}
-        >
-          <Route path=":username" element={<GithubUserSearch />} />
+        <Route exact path="/" element={<GithubUserSearch />}>
+          <Route exact path="/:username" element={<GithubUserSearch />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
