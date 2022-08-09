a3b6f1137afd6c1e5fd4f1d163ddc2b579e440d7
Marcele Monteiro <marcelepmonteiro@gmail.com>
2022-08-08 22:24:19 -0300

    Add link to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import "./Header.css";
 import iconMoon from "../assets/icon-moon.svg";
 import iconSun from "../assets/icon-sun.svg";
+import { Link } from "react-router-dom";
 
 function Header({ theme, setTheme }) {
   const themeChosen = theme === "light" ? "dark" : "light";
 
   return (
     <header className="header">
-      <h1>devfinder</h1>
+      <Link to="/">
+        <h1>devfinder</h1>
+      </Link>
       <button className="themeSwitcher" onClick={() => setTheme(themeChosen)}>
         {themeChosen}
         <img
