ff0a961cdfbab51761f7690668f23ac0234e052a
Marcele Monteiro <marcelepmonteiro@gmail.com>
2022-08-08 22:03:21 -0300

    Add reponsive design

--- src/components/User.js
+++ src/components/User.js
@@ -20,48 +20,48 @@ function User({ user }) {
       <div className="userAvatar">
         <img src={user.avatar_url} alt="User Avatar" />
       </div>
-      <div className="userInfo">
+      <div className="userHeader">
         <div className="userName">
           <h2>{user.name}</h2>
           <span>@{user.login}</span>
         </div>
         <p className="userCreatedDay">Joined {formatDate(user.created_at)}</p>
-        <p className="userBio">
-          {user.bio ? user.bio : "This profile has no bio"}
-        </p>
-        <div className="userNumbers">
-          <div className="userRepos">
-            <span>Repos</span>
-            <p>{user.public_repos}</p>
-          </div>
-          <div className="userFollowers">
-            <span>Followers</span>
-            <p>{user.followers}</p>
-          </div>
-          <div className="userFollowing">
-            <span>Following</span>
-            <p>{user.following}</p>
-          </div>
+      </div>
+      <p className="userBio">
+        {user.bio ? user.bio : "This profile has no bio"}
+      </p>
+      <div className="userNumbers">
+        <div className="userRepos">
+          <span>Repos</span>
+          <p>{user.public_repos}</p>
         </div>
-        <div className="userLinks">
-          <div className="userLocation">
-            <img src={locationIcon} alt="Location Icon" />
-            <span>{user.location}</span>
-          </div>
-          <div className="userTwitter">
-            <img src={twitterIcon} alt="Twitter Icon" />
-            <span>
-              {user.twitter_username ? user.twitter_username : "Not Available"}
-            </span>
-          </div>
-          <div className="userWebsite">
-            <img src={websiteIcon} alt="Website Icon" />
-            <span>{user.blog}</span>
-          </div>
-          <div className="userCompany">
-            <img src={companyIcon} alt="Company Icon" />
-            <span>{user.company}</span>
-          </div>
+        <div className="userFollowers">
+          <span>Followers</span>
+          <p>{user.followers}</p>
+        </div>
+        <div className="userFollowing">
+          <span>Following</span>
+          <p>{user.following}</p>
+        </div>
+      </div>
+      <div className="userLinks">
+        <div className="userLocation">
+          <img src={locationIcon} alt="Location Icon" />
+          <span>{user.location}</span>
+        </div>
+        <div className="userTwitter">
+          <img src={twitterIcon} alt="Twitter Icon" />
+          <span>
+            {user.twitter_username ? user.twitter_username : "Not Available"}
+          </span>
+        </div>
+        <div className="userWebsite">
+          <img src={websiteIcon} alt="Website Icon" />
+          <span>{user.blog}</span>
+        </div>
+        <div className="userCompany">
+          <img src={companyIcon} alt="Company Icon" />
+          <span>{user.company}</span>
         </div>
       </div>
     </div>
